ee44f67f19e49eab819a3c0c6de3627fa62aa7b9
Jay Momnang <juliusmomnang@gmail.com>
2022-06-04 13:10:27 +0100

    gitignore

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
 require("dotenv").config();
 const {MongoClient} = require('mongodb');
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+
+async function main() {
+	// we'll add code here soon
+
+  const uri = process.env.DB_URI;
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+  } catch (e) {
+    console.error(e);
+  }
+  finally {
+    await client.close();
+  }
+
+}
+
+main().catch(console.error);

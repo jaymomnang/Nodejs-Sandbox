1999468b3462f8bf552a756dcbd34e460fe832ab
Jay Momnang <juliusmomnang@gmail.com>
2022-06-06 12:17:23 +0100

    retrieve data limited to 50 - done

--- server.js
+++ server.js
@@ -14,15 +14,22 @@ async function main() {
   try {
     await client.connect();
     //databasesList = await client.db().apene().listDatabases();
-    const coas = client.db("apene").collection("COA");
+    const database = client.db("apenedb");
+    const coas = database.collection("COA");
     const query = {};
     const options = {
       sort: { acct: -1 },
       projection: { _id: 0, acct: 1, Description: 1 }
     };
     console.log("loading records.......");
-    records = coas.find(query, options).toArray();
-    console.log(records);
+    records = coas.find(query, options).limit(50);
+
+    // print a message if no documents were found
+    if ((await records.count()) === 0) {
+      console.log("No documents found!");
+    }
+
+    await records.forEach(console.dir);
 
   } catch (e) {
     console.error(e);
@@ -33,4 +40,4 @@ async function main() {
 
 }
 
-main().catch(console.error);
+main().catch(console.dir);
